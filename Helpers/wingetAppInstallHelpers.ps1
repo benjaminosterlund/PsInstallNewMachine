c1694d76faa94ba49d91a4fbbf3bfe9695e936c3
benjaminosterlund <osterlund_benjamin@hotmail.com>
2026-05-13 22:53:16 +0200

    Enhance winget installation process with improved argument handling and add installArgs for Docker Desktop

--- Helpers/wingetAppInstallHelpers.ps1
+++ Helpers/wingetAppInstallHelpers.ps1
@@ -31,12 +31,24 @@ function Install-AppFromWinget
         Write-Verbose "[winget] Installing '$($App.name)' with id '$effectiveId'"
         Write-Host "Installing: $($App.name)"
 
-        & winget install `
-            -e `
-            -h `
-            --accept-source-agreements `
-            --accept-package-agreements `
-            --id $effectiveId
+        $wingetArgs = @(
+            'install'
+            '-e'
+            '-h'
+            '--accept-source-agreements'
+            '--accept-package-agreements'
+            '--id', $effectiveId
+        )
+
+        if ($App.PSObject.Properties['wingetArgs'] -and $App.wingetArgs.Count -gt 0) {
+            $wingetArgs += [string[]]$App.wingetArgs
+        }
+
+        if ($App.PSObject.Properties['installArgs'] -and $App.installArgs.Count -gt 0) {
+            $wingetArgs += '--override', ([string[]]$App.installArgs -join ' ')
+        }
+
+        & winget @wingetArgs
 
         if ($LASTEXITCODE -ne 0) {
             Write-Error `
